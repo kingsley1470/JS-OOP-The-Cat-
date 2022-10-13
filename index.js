1d9ee42f982a7f68695311b9faf6cf439250800a
kingsley <rawmetal77@gmail.com>
2022-10-13 14:23:36 +0200

    commet added

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 
+
+
+
+
+/*
+Create a class that represents a cat. It should have properties for tiredness, hunger, loneliness, and happiness
+Next, write methods that increase and decrease those properties. Call them something that actually represents what would increase or decrease these things, like “feed”, “sleep”, or “pet”.
+Last, write a method that prints out the cat’s status in each area. (Be creative e.g. Paws is really hungry, Paws is VERY happy.)
+Bonus: Make the functions take arguments that increase or decrease arbitrary amounts
+
+*/
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Cat {
   constructor(name, tiredness = 0, hunger = 10, loneliness = 10, happiness = 0) {
     this.name = name;
